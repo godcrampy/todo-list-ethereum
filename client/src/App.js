1f7bfbf79fec20aaa1248591c1fabc4fd03f830d
Sahil <sahilbondre@gmail.com>
2019-12-17 13:21:28 +0530

    Add sass and bulme

--- client/src/App.js
+++ client/src/App.js
@@ -2,21 +2,26 @@ import React from "react";
 import { DrizzleContext } from "drizzle-react";
 
 import List from "./components/List";
+import "bulma/bulma.sass";
+import "./App.scss";
 
 class App extends React.Component {
   render() {
     return (
-      <DrizzleContext.Consumer>
-        {drizzleContext => {
-          const { drizzle, drizzleState, initialized } = drizzleContext;
+      <div className="App">
+        <h1 className="title is-1">Todo List</h1>
+        <DrizzleContext.Consumer>
+          {drizzleContext => {
+            const { drizzle, drizzleState, initialized } = drizzleContext;
 
-          if (!initialized) {
-            return "Loading...";
-          }
+            if (!initialized) {
+              return "Loading...";
+            }
 
-          return <List drizzle={drizzle} drizzleState={drizzleState} />;
-        }}
-      </DrizzleContext.Consumer>
+            return <List drizzle={drizzle} drizzleState={drizzleState} />;
+          }}
+        </DrizzleContext.Consumer>
+      </div>
     );
   }
 }
