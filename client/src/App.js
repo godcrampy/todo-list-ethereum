6fa4b66c18cf7ddd1e15ba994083d80881f38dc9
Sahil <sahilbondre@gmail.com>
2019-12-17 09:12:29 +0530

    Setup drizzle

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,25 @@
 import React from "react";
 
 class App extends React.Component {
+  state = { loading: true, drizzleState: null };
+  componentDidMount() {
+    const { drizzle } = this.props;
+    this.unsubscribe = drizzle.store.subscribe(() => {
+      // every time the store updates, grab the state from drizzle
+      const drizzleState = drizzle.store.getState();
+
+      // check to see if it's ready, if so, update local component state
+      if (drizzleState.drizzleStatus.initialized) {
+        this.setState({ loading: false, drizzleState });
+      }
+    });
+  }
+  componentWillUnmount() {
+    this.unsubscribe();
+  }
   render() {
-    return <div className="App">App</div>;
+    if (this.state.loading) return "Loading Drizzle...";
+    return <div className="App">Drizzle is ready</div>;
   }
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,18 @@
 import React from "react";
 import ReactDom from "react-dom";
+import { Drizzle } from "drizzle";
+import TodoList from "./contracts/TodoList.json";
 
 import App from "./App";
+const options = {
+  contracts: [TodoList],
+  web3: {
+    fallback: {
+      type: "ws",
+      url: "ws://127.0.0.1:7545"
+    }
+  }
+};
 
-ReactDom.render(<App />, document.getElementById("root"));
+const drizzle = new Drizzle(options);
+ReactDom.render(<App drizzle={drizzle} />, document.getElementById("root"));
