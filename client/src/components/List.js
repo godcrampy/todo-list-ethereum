f4dfa7b71c79da77b45616ea698b0fdecf03f5b1
Sahil <sahilbondre@gmail.com>
2019-12-17 11:53:57 +0530

    Display todos

--- client/src/components/List.js
+++ client/src/components/List.js
@@ -1,18 +1,35 @@
 import React from "react";
 
 class List extends React.Component {
-  state = { dataKey: null };
+  state = { dataKey: null, keys: [] };
   componentDidMount() {
     const { drizzle } = this.props;
     const contract = drizzle.contracts.TodoList;
-    
-    const dataKey = contract.methods["numberOfTodos"].cacheCall();
+
+    const dataKey = contract.methods.numberOfTodos.cacheCall();
     this.setState({ dataKey });
+    for (let i = 0; i < 10; ++i) {
+      const key = contract.methods.list.cacheCall(i);
+      this.setState(prevState => ({
+        keys: [...prevState.keys, key]
+      }));
+    }
   }
   render() {
     const { TodoList } = this.props.drizzleState.contracts;
     const todoListCount = TodoList.numberOfTodos[this.state.dataKey];
-    return <p>Number of Todos: {todoListCount && todoListCount.value}</p>;
+    return (
+      <div>
+        Number of Todos: {todoListCount && todoListCount.value}
+        <br />
+        Todos:
+        {this.state.keys.map((key, index) => {
+          const todo = TodoList.list[key];
+          if (todo && todo.value !== "") return <li key={index}>{todo.value}</li>;
+          return "";
+        })}
+      </div>
+    );
   }
 }
 
